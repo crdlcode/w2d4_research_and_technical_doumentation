0c1b2e32a0e43e1f3d71f9b6fc9478e07d6ebaf0
curtischatch <curtis.chat.hk@gmail.com>
2018-07-19 14:21:57 -0400

    finished

--- research.rb
+++ research.rb
@@ -71,8 +71,74 @@ a.first(2)  #=> ["q", "r"]
 # *****delete_at*****
 # Deletes the element at the specified index, returning that element, or nil if the index is out of range.
 # arguments: no arguments
-# return type: oject or nik
+# return type: object or nil
 # example:
 a = %w( ant bat cat dog )
 a.delete_at (2) #=> "cat"
 a               #=> ["ant","bat","dog"]
+
+# *******pop**********
+# description: will take a hash and convert it to a nested array where each element is a key value pair
+# arguments: none
+# return type: array
+# example:
+a = [ "a", "b", "c", "d" ]
+a.pop     #=> "d"
+a.pop(2)  #=> ["b", "c"]
+a         #=> ["a"]
+
+# ----------------------Hash---------------------------
+# to_a
+# description:
+# arguments: (integer amount)
+# return type: object/s or nil
+# example:
+hash1 = {"c" => 40, "a" => 20, "b" => 10}.to_a #=> [["c", 40], ["a", 20], ["b", 10]]
+
+# has_key?
+# description: converts and object into an array
+# arguments: (object key_req)
+# return type: boolean
+# example:
+
+h = { "a" => 100, "b" => 200 }
+h.has_key?("a")   #=> true
+h.has_key?("z")   #=> false
+
+# has_value
+# description: returns true if given value is present for some key in hsh
+# arguments: (object val_req)
+# return type: boolean
+# example:
+
+h = { "a" => 100, "b" => 200 }
+h.has_value?(100)   #=> true
+h.has_value?(999)   #=> false
+
+#----------------------Time-------------------------
+#now
+# description: returns the current date/time
+# arguments: none
+# return type: object time
+# example:
+
+puts = "#{time.now}"
+
+#----------------------File-------------------------
+
+#   * ```exist?```
+#       Arguments: (string filename)
+#       Return type: Boolean
+#       Exmaple code of how to use method:
+puts "********* .exist *********"
+filereal = "./exercise.rb"
+filefake = "./exercise2.rb"
+puts "Does #{filereal} exist? #{File.exist?(filereal)}"
+puts "Does #{filefake} exist? #{File.exist?(filefake)}"
+
+#   * ```extname```
+#       Arguments: (string filename)
+#       Return type: string extension name
+#       Exmaple code of how to use method:
+puts "********* .extname *********"
+puts "The extension of \".\/exercise.rb\" is #{File.extname("./exercise.rb")}"
